42ef2e5e685d09db517f4ea31f4ba688b9c166c2
shubham9049 <shubham9049>
2025-06-16 18:21:12 +0530

    add more cgi

--- src/data/postData.ts
+++ src/data/postData.ts
@@ -2,6 +2,9 @@ import cgi1 from "../assets/gallery/cgi1.png";
 import cgi2 from "../assets/gallery/cgi2.png";
 import cgi3 from "../assets/gallery/cgi3.png";
 import cgi4 from "../assets/gallery/cgi4.png";
+import cgi5 from "../assets/gallery/ChatGPT Image Jun 3, 2025, 04_36_48 PM.png";
+import cgi6 from "../assets/gallery/ChatGPT Image Jun 3, 2025, 06_27_27 PM.png";
+import cgi7 from "../assets/gallery/ChatGPT Image May 31, 2025, 02_57_46 PM.png";
 import static1 from "../assets/gallery/BigWig Static Post.png";
 import static2 from "../assets/gallery/BigWig Static Post (1).png";
 import static3 from "../assets/gallery/Bigwig Static Post (2).png";
@@ -36,6 +39,24 @@ export const postItems: PostItem[] = [
   },
   {
     src: cgi4,
+    alt: "CGI Post",
+    link: "https://www.instagram.com/bigwigmediadigital/?igsh=bWJ3dXN1cGxrbW5u#",
+    type: "cgi",
+  },
+  {
+    src: cgi5,
+    alt: "CGI Post",
+    link: "https://www.instagram.com/bigwigmediadigital/?igsh=bWJ3dXN1cGxrbW5u#",
+    type: "cgi",
+  },
+  {
+    src: cgi6,
+    alt: "CGI Post",
+    link: "https://www.instagram.com/bigwigmediadigital/?igsh=bWJ3dXN1cGxrbW5u#",
+    type: "cgi",
+  },
+  {
+    src: cgi7,
     alt: "CGI Post",
     link: "https://www.instagram.com/bigwigmediadigital/?igsh=bWJ3dXN1cGxrbW5u#",
     type: "cgi",
