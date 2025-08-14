b4fc700b89e0ff011c7b380c75b02adf57583037
unknown <chandanpms@gmail.com>
2025-08-14 12:30:14 +0530

    updated

--- src/components/AddBlogs.tsx
+++ src/components/AddBlogs.tsx
@@ -163,7 +163,7 @@ const AddBlog = ({
   return (
     <div className="fixed inset-0 bg-black bg-opacity-60 flex items-center justify-center z-50">
       <div className="bg-white text-black p-6 w-full max-w-2xl rounded-xl overflow-y-auto max-h-[90vh]">
-        <h2 className="text-2xl font-bold mb-4">
+        <h2 className="text-2xl font-bold mb-4 text-black">
           {existingBlog ? "Edit Blog" : "Add New Blog"}
         </h2>
         <form
